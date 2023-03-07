fa4fc82b1c1acd155a8f27628c18ab8fb8cd6a23
Jefferson Dada <125513220+xnaps@users.noreply.github.com>
2023-03-07 19:40:31 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Função sem retorno
+                    //parametros
+function imprimirSoma(a,b)(
+  let resultado = a + b
+  console.log(resultado)
+)
+
+let numero2 = 10
+let numero3 = 11
+imprimirSoma(numero2,numero3)
+imprimirSoma(2)
+imprimirSoma(2.5, 5 , 6 , 7 , 5.6 ,9)
+imprimirSoma()
+//função com retorno
+//os parametros podem ou não receber valor por padrao
+function soma(a - 5, b - 10){
+  let resultado - console.log(a+b)
+  return resultado
+}
+soma()
